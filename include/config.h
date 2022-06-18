2bc89450d00e90332deaba4c63f14cfe341a6daa
leoNavarro95 <potty3800@gmail.com>
2022-06-18 08:58:21 -0500

    Project working. Added littlefs. Updated ESP Core

---
+++ include/config.h
@@ -0,0 +1,7 @@
+const char* ssid     = "Anonymous";
+const char* password = "12345678";
+const char* hostname = "ESP32_1";
+
+IPAddress ip(192, 168, 1, 200);
+IPAddress gateway(192, 168, 1, 1);
+IPAddress subnet(255, 255, 255, 0);
