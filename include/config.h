47d5969763c38064b4cac9e1c40d62060d2c2100
leoNavarro95 <potty3800@gmail.com>
2022-06-21 15:57:49 -0500

    added other ssid and pass

--- include/config.h
+++ include/config.h
@@ -1,5 +1,8 @@
 const char* ssid     = "Anonymous";
 const char* password = "12345678";
+// const char* ssid     = "TP-LINK_FD98";
+// const char* password = "19225685";
+
 const char* hostname = "ESP32_1";
 
 IPAddress ip(192, 168, 1, 200);
