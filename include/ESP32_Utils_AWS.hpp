4c4a0e91c64e29f132f09cdbbd167523d3f29e19
leoNavarro95 <potty3800@gmail.com>
2022-06-21 15:58:54 -0500

    Formated Hello Client message in json Uncommented ws debbuging messages

--- include/ESP32_Utils_AWS.hpp
+++ include/ESP32_Utils_AWS.hpp
@@ -1,14 +1,14 @@
 void onWsEvent(AsyncWebSocket * server, AsyncWebSocketClient * client, AwsEventType type, void * arg, uint8_t *data, size_t len){ 
 	if(type == WS_EVT_CONNECT){
-		//Serial.printf("ws[%s][%u] connect\n", server->url(), client->id());
-		client->printf("Hello Client %u :)", client->id());
+		Serial.printf("ws[%s][%u] connect\n", server->url(), client->id());
+		client->printf("{\"message\": \"Hello Client %u\"}", client->id());
 		client->ping();
 	} else if(type == WS_EVT_DISCONNECT){
-		//Serial.printf("ws[%s][%u] disconnect: %u\n", server->url(), client->id());
+		Serial.printf("ws[%s][%u] disconnect: %u\n", server->url(), client->id());
 	} else if(type == WS_EVT_ERROR){
-		//Serial.printf("ws[%s][%u] error(%u): %s\n", server->url(), client->id(), *((uint16_t*)arg), (char*)data);
+		Serial.printf("ws[%s][%u] error(%u): %s\n", server->url(), client->id(), *((uint16_t*)arg), (char*)data);
 	} else if(type == WS_EVT_PONG){
-		//Serial.printf("ws[%s][%u] pong[%u]: %s\n", server->url(), client->id(), len, (len)?(char*)data:"");
+		Serial.printf("ws[%s][%u] pong[%u]: %s\n", server->url(), client->id(), len, (len)?(char*)data:"");
 	} else if(type == WS_EVT_DATA){
 		AwsFrameInfo * info = (AwsFrameInfo*)arg;
 		String msg = "";
