bbc4e4b4d693aca74b523e29c72a90a2ce8a16fc
leoNavarro95 <potty3800@gmail.com>
2022-07-12 16:04:01 -0500

    Added macros for size of static arrays Using snprintf to move secure char arrays

--- include/SmartDevice_Model.hpp
+++ include/SmartDevice_Model.hpp
@@ -8,6 +8,12 @@
 #define MAX_SIZE_SSID 32
 #define MAX_SIZE_PASS 31
 
+#define MAX_SIZE_IP_MODE 4
+#define MAX_SIZE_IP_ADDRESS 15
+#define MAX_SIZE_IP_MASK 15
+#define MAX_SIZE_IP_GATEWAY 15
+
+
 #define MAX_SIZE_USED_GPIOS 30
 #define SIZE_GPIOS 30
 
@@ -41,24 +47,24 @@ class IpConfig {
     virtual ~IpConfig() = default;
 
     private:
-    char mode[5]; //Static|Dynamic
-    char ip_address[16]; // 111.111.111.111
-    char subred_mask_address[16];
-    char gateway_address[16];
+    char mode[MAX_SIZE_IP_MODE]; //Static|Dynamic
+    char ip_address[MAX_SIZE_IP_ADDRESS]; // 111.111.111.111
+    char subred_mask_address[MAX_SIZE_IP_MASK];
+    char gateway_address[MAX_SIZE_IP_GATEWAY];
     // TODO: DNS address
 
     public:
     const char* get_mode() const { return mode; }
-    void set_mode(const char* value) { strcpy(this->mode, value);}
+    void set_mode(const char* value) { snprintf(this->mode, MAX_SIZE_IP_MODE, value);} // strcpy(this->mode, value); 
 
     const char* get_ip_address() const { return ip_address; }
-    void set_ip_address(const char* value) { strcpy(this->ip_address, value);}
+    void set_ip_address(const char* value) { snprintf(this->ip_address, MAX_SIZE_IP_ADDRESS, value);}
 
     const char* get_subred_mask_address() const { return subred_mask_address; }
-    void set_subred_mask_address(const char* value) { strcpy(this->subred_mask_address, value);}
+    void set_subred_mask_address(const char* value) { snprintf(this->subred_mask_address, MAX_SIZE_IP_MASK, value);}
     
     const char* get_gateway_address() const { return gateway_address; }
-    void set_gateway_address(const char* value) { strcpy(this->gateway_address, value);}
+    void set_gateway_address(const char* value) { snprintf(this->gateway_address, MAX_SIZE_IP_GATEWAY, value);}
 
 };
 
@@ -70,9 +76,9 @@ class UsedGpio {
     private:
     uint8_t id;
     uint8_t pin_number;
-    char mode[MAX_SIZE_MODE + 1];     //OUTPUT | INPUT | ANALOG | PWM...
-    char label[MAX_SIZE_LABEL + 1];    //Bombillo sala
-    char value[MAX_SIZE_VALUE + 1];    // ON | OFF | true | false | 1023 | 255
+    char mode[MAX_SIZE_MODE];     //OUTPUT | INPUT | ANALOG | PWM...
+    char label[MAX_SIZE_LABEL];    //Bombillo sala
+    char value[MAX_SIZE_VALUE];    // ON | OFF | true | false | 1023 | 255
 
     public:
     const uint8_t & get_id() const { return id; }
@@ -84,13 +90,13 @@ class UsedGpio {
     void set_pin_number(const uint8_t & value) { this->pin_number = value; }
 
     const char * get_mode() { return mode; }
-    void set_mode(const char * value) { strcpy(this->mode, value); }
+    void set_mode(const char * value) { snprintf(this->mode, MAX_SIZE_MODE, value); }
 
     const char * get_label() { return label; }
-    void set_label(const char * value) { strcpy(this->label, value); }
+    void set_label(const char * value) { snprintf(this->label, MAX_SIZE_LABEL, value); }
 
     const char * get_value() { return value; }
-    void set_value(const char * value) { strcpy(this->value, value); }
+    void set_value(const char * value) { snprintf(this->value, MAX_SIZE_VALUE, value); }
 };
 
 class SmartDevice {
@@ -99,13 +105,13 @@ class SmartDevice {
     virtual ~SmartDevice() = default;
 
     private:
-    char mac[SIZE_MAC + 1]; //11:00:5e:00:53:af
-    char ap_ssid[MAX_SIZE_SSID + 1];
-    char ap_pass[MAX_SIZE_PASS + 1];
-    char sta_ssid[MAX_SIZE_SSID + 1];
-    char sta_pass[MAX_SIZE_PASS + 1];
+    char mac[SIZE_MAC]; //11:00:5e:00:53:af
+    char ap_ssid[MAX_SIZE_SSID];
+    char ap_pass[MAX_SIZE_PASS];
+    char sta_ssid[MAX_SIZE_SSID];
+    char sta_pass[MAX_SIZE_PASS];
     // TODO: mode multiAP
-    char connection_mode [MAX_SIZE_CONN_MODE + 1]; // 'STA'|'AP' 
+    char connection_mode [MAX_SIZE_CONN_MODE]; // 'STA'|'AP' 
     
     IpConfig ip_config;
     UsedGpio used_gpios[MAX_SIZE_USED_GPIOS];
@@ -113,22 +119,22 @@ class SmartDevice {
 
     public:
     const char* get_mac() const { return mac; }
-    void set_mac(const char * value) { strcpy(this->mac, value);}
+    void set_mac(const char * value) { snprintf(this->mac, SIZE_MAC, value);}
 
     const char * get_ap_ssid() { return ap_ssid; }
-    void set_ap_ssid(const char * value) { strcpy(this->ap_ssid, value); }
+    void set_ap_ssid(const char * value) { snprintf(this->ap_ssid, MAX_SIZE_SSID, value); }
 
     const char * get_ap_pass() { return ap_pass; }
-    void set_ap_pass(const char * value) { strcpy(this->ap_pass, value); }
+    void set_ap_pass(const char * value) { snprintf(this->ap_pass, MAX_SIZE_PASS, value); }
 
     const char * get_sta_ssid() { return sta_ssid; }
-    void set_sta_ssid(const char * value) { strcpy(this->sta_ssid, value); }
+    void set_sta_ssid(const char * value) { snprintf(this->sta_ssid, MAX_SIZE_SSID, value);}
 
     const char * get_sta_pass() { return sta_pass; }
-    void set_sta_pass(const char * value) { strcpy(this->sta_pass, value); }
+    void set_sta_pass(const char * value) { snprintf(this->sta_pass, MAX_SIZE_PASS, value);}
 
     const char* get_connection_mode() const { return connection_mode; }
-    void set_connection_mode(const char* value) { strcpy(this->connection_mode, value);}
+    void set_connection_mode(const char* value) { snprintf(this->connection_mode, MAX_SIZE_CONN_MODE, value);}
 
     const IpConfig & get_ip_config() const { return ip_config; }
     IpConfig & get_mutable_ip_config() { return ip_config; }
@@ -137,7 +143,7 @@ class SmartDevice {
     UsedGpio* get_used_gpios() { return used_gpios;}
     
     void set_used_gpios(UsedGpio* value, uint8_t size) {
-        if(size > MAX_SIZE_USED_GPIOS){
+        if(size >= MAX_SIZE_USED_GPIOS){
             Serial.printf("[SmartD][E]- UsedGpio array size is bigger than reserved");
             return;
         } 
@@ -148,7 +154,7 @@ class SmartDevice {
 
     Gpio* get_gpios() { return gpios; }
     void set_gpios(Gpio* value, uint8_t size) { 
-        if(size > SIZE_GPIOS){
+        if(size >= SIZE_GPIOS){
             Serial.printf("[SmartD][E]- Gpio array size is bigger than reserved");
             return;
         }
@@ -160,8 +166,46 @@ class SmartDevice {
     // const std::vector<UsedGpio> & get_used_gpios() const { return used_gpios; }
     // std::vector<UsedGpio> & get_mutable_used_gpios() { return used_gpios; }
     // void set_used_gpios(const std::vector<UsedGpio> & value) { this->used_gpios = value; }
-
-    // const std::vector<Gpio> & get_gpios() const { return gpios; }
-    // std::vector<Gpio> & get_mutable_gpios() { return gpios; }
-    // void set_gpios(const std::vector<Gpio> & value) { this->gpios = value; }
 };
+
+
+// DEMO USING THIS CLASS
+/*
+  if(DB.begin("/database/esp_soc.json"))
+    DB.initDeviceFromDB(sdevice);
+
+  Serial.printf("ap pass: %s\n", sdevice.get_ap_pass());
+  Serial.printf("ap ssid: %s\n", sdevice.get_ap_ssid());
+  Serial.printf("conncetion mode: %s\n", sdevice.get_connection_mode());
+  Serial.printf("mac: %s\n", sdevice.get_mac());
+  Serial.printf("sta pass: %s\n", sdevice.get_sta_pass());
+  Serial.printf("sta ssid: %s\n", sdevice.get_sta_ssid());
+
+  UsedGpio * used_gpios = sdevice.get_used_gpios(); 
+  uint8_t size_u_gpios = DB.get_size_used_gpios();
+  
+
+  for(int i = 0; i < size_u_gpios; i++){
+    Serial.println("++++++++++++++++++++++++++++++++++");
+    Serial.printf("id: %d\n", used_gpios[i].get_id());
+    Serial.printf("pin: %d\n", used_gpios[i].get_pin_number());
+    Serial.printf("mode: %s\n", used_gpios[i].get_mode());
+    Serial.printf("label: %s\n", used_gpios[i].get_label());
+    Serial.printf("value: %s\n", used_gpios[i].get_value());
+    Serial.println("++++++++++++++++++++++++++++++++++");
+    Serial.println();
+  }
+
+  Gpio * gpios = sdevice.get_gpios(); 
+  uint8_t size_gpios = DB.get_size_gpios();
+  
+
+  for(int i = 0; i < size_gpios; i++){
+    Serial.println("====================================");
+    Serial.printf("id: %d\n", gpios[i].get_id());
+    Serial.printf("pin: %d\n", gpios[i].get_pin_number());
+    Serial.printf("used: %s\n", (gpios[i].get_used() ? "yes" : "no"));
+    Serial.println("====================================");
+    Serial.println();
+  }
+*/
