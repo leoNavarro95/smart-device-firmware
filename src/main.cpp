d4ebf1061059457cffa3fc7a75f4440692df0731
leoNavarro95 <potty3800@gmail.com>
2022-07-12 16:04:30 -0500

    Removed test code

--- src/main.cpp
+++ src/main.cpp
@@ -32,40 +32,7 @@ void setup(void)
   if(DB.begin("/database/esp_soc.json"))
     DB.initDeviceFromDB(sdevice);
 
-  Serial.printf("ap pass: %s\n", sdevice.get_ap_pass());
-  Serial.printf("ap ssid: %s\n", sdevice.get_ap_ssid());
-  Serial.printf("conncetion mode: %s\n", sdevice.get_connection_mode());
-  Serial.printf("mac: %s\n", sdevice.get_mac());
-  Serial.printf("sta pass: %s\n", sdevice.get_sta_pass());
-  Serial.printf("sta ssid: %s\n", sdevice.get_sta_ssid());
 
-  UsedGpio * used_gpios = sdevice.get_used_gpios(); 
-  uint8_t size_u_gpios = DB.get_size_used_gpios();
-  
-
-  for(int i = 0; i < size_u_gpios; i++){
-    Serial.println("++++++++++++++++++++++++++++++++++");
-    Serial.printf("id: %d\n", used_gpios[i].get_id());
-    Serial.printf("pin: %d\n", used_gpios[i].get_pin_number());
-    Serial.printf("mode: %s\n", used_gpios[i].get_mode());
-    Serial.printf("label: %s\n", used_gpios[i].get_label());
-    Serial.printf("value: %s\n", used_gpios[i].get_value());
-    Serial.println("++++++++++++++++++++++++++++++++++");
-    Serial.println();
-  }
-
-  Gpio * gpios = sdevice.get_gpios(); 
-  uint8_t size_gpios = DB.get_size_gpios();
-  
-
-  for(int i = 0; i < size_gpios; i++){
-    Serial.println("====================================");
-    Serial.printf("id: %d\n", gpios[i].get_id());
-    Serial.printf("pin: %d\n", gpios[i].get_pin_number());
-    Serial.printf("used: %s\n", (gpios[i].get_used() ? "yes" : "no"));
-    Serial.println("====================================");
-    Serial.println();
-  }
 
 
 /*
