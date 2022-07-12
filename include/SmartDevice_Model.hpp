035cb70385f33c4fac5273bdc6cd1ddaee040667
leoNavarro95 <potty3800@gmail.com>
2022-07-12 09:25:37 -0500

    Optimized data members types Migrated from string to char array and from vector to object array

--- include/SmartDevice_Model.hpp
+++ include/SmartDevice_Model.hpp
@@ -34,27 +34,25 @@ class IpConfig {
     virtual ~IpConfig() = default;
 
     private:
-    std::string mode;
-    std::string ip_address;
-    std::string subred_mask_address;
-    std::string gateway_address;
+    char mode[5]; //Static|Dynamic
+    char ip_address[16]; // 111.111.111.111
+    char subred_mask_address[16];
+    char gateway_address[16];
+    // TODO: DNS address
 
     public:
-    const std::string & get_mode() const { return mode; }
-    std::string & get_mutable_mode() { return mode; }
-    void set_mode(const std::string & value) { this->mode = value; }
+    const char* get_mode() const { return mode; }
+    void set_mode(const char* value) { strcpy(this->mode, value);}
 
-    const std::string & get_ip_address() const { return ip_address; }
-    std::string & get_mutable_ip_address() { return ip_address; }
-    void set_ip_address(const std::string & value) { this->ip_address = value; }
+    const char* get_ip_address() const { return ip_address; }
+    void set_ip_address(const char* value) { strcpy(this->ip_address, value);}
 
-    const std::string & get_subred_mask_address() const { return subred_mask_address; }
-    std::string & get_mutable_subred_mask_address() { return subred_mask_address; }
-    void set_subred_mask_address(const std::string & value) { this->subred_mask_address = value; }
+    const char* get_subred_mask_address() const { return subred_mask_address; }
+    void set_subred_mask_address(const char* value) { strcpy(this->subred_mask_address, value);}
+    
+    const char* get_gateway_address() const { return gateway_address; }
+    void set_gateway_address(const char* value) { strcpy(this->gateway_address, value);}
 
-    const std::string & get_gateway_address() const { return gateway_address; }
-    std::string & get_mutable_gateway_address() { return gateway_address; }
-    void set_gateway_address(const std::string & value) { this->gateway_address = value; }
 };
 
 class UsedGpio {
@@ -65,9 +63,9 @@ class UsedGpio {
     private:
     uint8_t id;
     uint8_t pin_number;
-    std::string mode;
-    std::string label;
-    std::string value;
+    const char* mode;
+    const char* label;
+    const char* value;
 
     public:
     const uint8_t & get_id() const { return id; }
@@ -78,17 +76,14 @@ class UsedGpio {
     uint8_t & get_mutable_pin_number() { return pin_number; }
     void set_pin_number(const uint8_t & value) { this->pin_number = value; }
 
-    const std::string & get_mode() const { return mode; }
-    std::string & get_mutable_mode() { return mode; }
-    void set_mode(const std::string & value) { this->mode = value; }
+    const char * get_mode() { return mode; }
+    void set_mode(const char * value) { this->mode = value; }
 
-    const std::string & get_label() const { return label; }
-    std::string & get_mutable_label() { return label; }
-    void set_label(const std::string & value) { this->label = value; }
+    const char * get_label() { return label; }
+    void set_label(const char * value) { this->label = value; }
 
-    const std::string & get_value() const { return value; }
-    std::string & get_mutable_value() { return value; }
-    void set_value(const std::string & value) { this->value = value; }
+    const char * get_value() { return value; }
+    void set_value(const char * value) { this->value = value; }
 };
 
 class SmartDevice {
@@ -97,50 +92,53 @@ class SmartDevice {
     virtual ~SmartDevice() = default;
 
     private:
-    std::string mac;
-    std::string ap_ssid;
-    std::string ap_pass;
-    std::string sta_ssid;
-    std::string sta_pass;
-    std::string connection_mode;
+    char mac[18]; //11:00:5e:00:53:af
+    const char * ap_ssid;
+    const char * ap_pass;
+    const char * sta_ssid;
+    const char * sta_pass;
+    // TODO: mode multiAP
+    char connection_mode [5]; // 'STA'|'AP' 
+    
     IpConfig ip_config;
-    std::vector<UsedGpio> used_gpios;
-    std::vector<Gpio> gpios;
+    UsedGpio * used_gpios;
+    Gpio * gpios;
 
     public:
-    const std::string & get_mac() const { return mac; }
-    std::string & get_mutable_mac() { return mac; }
-    void set_mac(const std::string & value) { this->mac = value; }
+    // char* get_mutable_mac() { return mac; }
+    const char* get_mac() const { return mac; }
+    void set_mac(const char* value) { strcpy(this->mac, value);}
 
-    const std::string & get_ap_ssid() const { return ap_ssid; }
-    std::string & get_mutable_ap_ssid() { return ap_ssid; }
-    void set_ap_ssid(const std::string & value) { this->ap_ssid = value; }
+    const char * get_ap_ssid() { return ap_ssid; }
+    void set_ap_ssid(const char * value) { this->ap_ssid = value; }
 
-    const std::string & get_ap_pass() const { return ap_pass; }
-    std::string & get_mutable_ap_pass() { return ap_pass; }
-    void set_ap_pass(const std::string & value) { this->ap_pass = value; }
+    const char * get_ap_pass() { return ap_pass; }
+    void set_ap_pass(const char * value) { this->ap_pass = value; }
 
-    const std::string & get_sta_ssid() const { return sta_ssid; }
-    std::string & get_mutable_sta_ssid() { return sta_ssid; }
-    void set_sta_ssid(const std::string & value) { this->sta_ssid = value; }
+    const char * get_sta_ssid() { return sta_ssid; }
+    void set_sta_ssid(const char * value) { this->sta_ssid = value; }
 
-    const std::string & get_sta_pass() const { return sta_pass; }
-    std::string & get_mutable_sta_pass() { return sta_pass; }
-    void set_sta_pass(const std::string & value) { this->sta_pass = value; }
+    const char * get_sta_pass() { return sta_pass; }
+    void set_sta_pass(const char * value) { this->sta_pass = value; }
 
-    const std::string & get_connection_mode() const { return connection_mode; }
-    std::string & get_mutable_connection_mode() { return connection_mode; }
-    void set_connection_mode(const std::string & value) { this->connection_mode = value; }
+    const char* get_connection_mode() const { return connection_mode; }
+    void set_connection_mode(const char* value) { strcpy(this->connection_mode, value);}
 
     const IpConfig & get_ip_config() const { return ip_config; }
     IpConfig & get_mutable_ip_config() { return ip_config; }
     void set_ip_config(const IpConfig & value) { this->ip_config = value; }
+    
+    UsedGpio* get_used_gpios() { return used_gpios;}
+    void set_used_gpios(UsedGpio* value) { this-> used_gpios = value;}
 
-    const std::vector<UsedGpio> & get_used_gpios() const { return used_gpios; }
-    std::vector<UsedGpio> & get_mutable_used_gpios() { return used_gpios; }
-    void set_used_gpios(const std::vector<UsedGpio> & value) { this->used_gpios = value; }
+    Gpio* get_gpios() { return gpios; }
+    void set_gpios(Gpio* value) { this-> gpios = value; }
 
-    const std::vector<Gpio> & get_gpios() const { return gpios; }
-    std::vector<Gpio> & get_mutable_gpios() { return gpios; }
-    void set_gpios(const std::vector<Gpio> & value) { this->gpios = value; }
+    // const std::vector<UsedGpio> & get_used_gpios() const { return used_gpios; }
+    // std::vector<UsedGpio> & get_mutable_used_gpios() { return used_gpios; }
+    // void set_used_gpios(const std::vector<UsedGpio> & value) { this->used_gpios = value; }
+
+    // const std::vector<Gpio> & get_gpios() const { return gpios; }
+    // std::vector<Gpio> & get_mutable_gpios() { return gpios; }
+    // void set_gpios(const std::vector<Gpio> & value) { this->gpios = value; }
 };
